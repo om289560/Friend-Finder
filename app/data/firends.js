1d732a0b0f286f966786e0d563a389bb96afc7e6
Oscar Miranda <omiranda0058@gmail.com>
2019-09-15 23:35:29 -0500

    added data

---
+++ app/data/firends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+        name: "Tommy Tim",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    },
+    {
+        name: "Jimmy Fallon",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    },
+    {
+        name: "Oliver Twist",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    },
+    {
+        name: "Mr. Rogers",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    },
+    {
+        name: "Bill Burr",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    },
+    {
+        name: "Travis Scott",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    },
+    {
+        name: "Bernie Sanders",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    },
+    {
+        name: "Adam Sandler",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    },
+    {
+        name: "Michael Scott",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    },
+    {
+        name: "John Wayne",
+        photo: "",
+        scores: [3, 4, 4, 4, 4, 2, 6, 7, 5, 5]
+    }
+]
--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+app.use(express.static("app/public"));
+
+// require("app/routing/apiRoutes")(app);
+// require("app/routing/htmlRoutes")(app);
+
+app.listen(port, () => console.log("listening on port %s", port));
 
